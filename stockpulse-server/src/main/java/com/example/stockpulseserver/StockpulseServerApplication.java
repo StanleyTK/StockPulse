2b7f741a88831d95ccf2a9f4956b5bfb2fd06d7b
stanleytk <kim4111@purdue.edu>
2024-05-18 20:38:40 -0400

    refactoring

--- stockpulse-server/src/main/java/com/example/stockpulseserver/StockpulseServerApplication.java
+++ stockpulse-server/src/main/java/com/example/stockpulseserver/StockpulseServerApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StockpulseServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StockpulseServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("Starting Stockpulse Server Application...");
+        SpringApplication.run(StockpulseServerApplication.class, args);
+        System.out.println("Yello - The application has started!");
+    }
 
 }
