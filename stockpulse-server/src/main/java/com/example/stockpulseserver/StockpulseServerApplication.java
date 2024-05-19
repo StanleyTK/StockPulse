5a0d79a40d403544df676d4e27bb82bff9df1933
stanleytk <kim4111@purdue.edu>
2024-05-18 20:12:04 -0400

    first commit

---
+++ stockpulse-server/src/main/java/com/example/stockpulseserver/StockpulseServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockpulseserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockpulseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockpulseServerApplication.class, args);
+	}
+
+}
---
+++ stockpulse-server/src/test/java/com/example/stockpulseserver/StockpulseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockpulseserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockpulseServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
