79b98ad66946a1e57c391ea0ea253b4db95c09dd
stanleytk <kim4111@purdue.edu>
2024-05-20 17:08:21 -0400

    Authorization System

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 p-4 text-center text-gray-400">
+      &copy; {new Date().getFullYear()} StockPulse. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
