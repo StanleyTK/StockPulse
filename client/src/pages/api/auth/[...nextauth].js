9da9d41fa387c3207bc98c10dbf17765cdc5572c
stanleytk <kim4111@purdue.edu>
2024-05-25 13:07:23 -0400

    fixed?

--- client/src/pages/api/auth/[...nextauth].js
+++ client/src/pages/api/auth/[...nextauth].js
@@ -51,5 +51,11 @@ export default NextAuth({
 
       return session;
     }
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60,
+    updateAge: 24 * 60 * 60,
   }
 });
