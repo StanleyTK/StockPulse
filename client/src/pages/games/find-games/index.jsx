eb6d071ee4778ef2ad73150545439ef345a50dac
stanleytk <kim4111@purdue.edu>
2024-05-25 13:09:45 -0400

    final commit

--- client/src/pages/games/find-games/index.jsx
+++ client/src/pages/games/find-games/index.jsx
@@ -4,14 +4,14 @@ import ProtectedRoute from '../../../components/ProtectedRoute';
 
 const FindGames = () => {
   return (
-    //<ProtectedRoute>
+    <ProtectedRoute>
       <Layout>
         <h1 className="text-2xl font-bold text-center my-4">Find Games</h1>
         <div className="max-w-md mx-auto bg-white p-8 shadow-md rounded">
-          <p className="text-center text-gray-700">Here you can find games.</p>
+          <p className="text-center text-gray-700">Here you can find games. </p>
         </div>
       </Layout>
-  //  </ProtectedRoute>
+   </ProtectedRoute>
   );
 };
 
